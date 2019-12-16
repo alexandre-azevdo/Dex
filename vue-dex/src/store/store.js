f846392a0f3852b8f3d9775d1f45f49b6afb9766
alexazv_ilha <alexandre.azevedo@ilhasoft.com.br>
2019-12-16 16:03:25 -0300

    Added util object

--- vue-dex/src/store/store.js
+++ vue-dex/src/store/store.js
@@ -2,43 +2,10 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
+import { Util } from './util.js' 
 
 Vue.use(Vuex)
 Vue.use(VueAxios, axios)
-
-const Util = {
-  
-  capitalizeFirstLetter: function (string) {
-    return string.charAt(0).toUpperCase() + string.slice(1)
-  },
-
-  image: function(index, isFavorite) {
-    return "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/" + (isFavorite ? "shiny/" : "") + index + ".png"
-  },
-
-  isFavorite: function(index) {
-    return localStorage.favorites && JSON.parse(localStorage.favorites).favoriteList.includes(index)
-  },
-
-  toggleFavorite: function(index) {
-    if(localStorage.favorites) {
-      let favorites = JSON.parse(localStorage.favorites)
-      if (favorites.favoriteList.includes(index))
-        favorites.favoriteList.splice(favorites.favoriteList.indexOf(index), 1)
-      else
-        favorites.favoriteList.push(index)
-          
-      localStorage.favorites = JSON.stringify(favorites)
-      
-      } else {
-      localStorage.favorites = JSON.stringify(
-                          {
-                              favoriteList: [index],
-                            }
-      )
-    }
-  }
-}
 
 export const store = new Vuex.Store ({
 
